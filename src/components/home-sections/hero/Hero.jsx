826b26ce661e993925af43d91355ad54444d3dc8
exize <amio121212@gmail.com>
2022-09-21 13:41:01 +0300

    hero-and-footer

--- src/components/home-sections/hero/Hero.jsx
+++ src/components/home-sections/hero/Hero.jsx
@@ -1,7 +1,15 @@
 
+import "./hero.css"
+
 const Hero = () => {
     return(
-        <></>
+        <div className="hero-container">
+            <h2 className="hero-sub-title">עשרות כלבים מחכים לבית</h2>
+            <h1 className="hero-title">בואו לאמץ חבר/ה</h1>
+            <div className="hero-btn-container">
+                <button className="hero-btn">אמצו אותנו</button>
+            </div>
+        </div>
     )
 }
 
--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -1,7 +1,16 @@
+import Footer from "../../components/footer/Footer"
+import About from "../../components/home-sections/about/About"
+import Hero from "../../components/home-sections/hero/Hero"
+import Navbar from "../../components/navbar/Navbar"
 
 const Home = () => {
     return(
-        <></>
+        <>
+            <Navbar/>
+            <Hero/>
+            <About/>
+            <Footer/>
+        </>
     )
 }
 
