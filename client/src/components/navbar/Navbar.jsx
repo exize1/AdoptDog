54c8c97e9cfea6ed49618c7b70d6a5fa959f90c4
exize <amio121212@gmail.com>
2022-09-21 22:43:20 +0300

    fixing-conflicts

--- client/src/components/navbar/Navbar.jsx
+++ client/src/components/navbar/Navbar.jsx
@@ -1,8 +1,45 @@
+import { Form } from 'formik';
+import Container from 'react-bootstrap/Container';
+import Navbar from 'react-bootstrap/Navbar';
+import css from "./navbar.css";
+import logo from "./logo1.png"
 
-const Navbar = () => {
-    return(
-        <></>
-    )
-}
 
-export default Navbar
+function MainNavbar() {
+    return (
+      <>
+  
+
+  <nav className="navbar bg-light mainnav ">
+  <div className="container navitems">
+    <a className="navbar-brand" href="#">
+    <img className='logoimg' src={logo} /> 
+    </a>
+    <h1 className='navphonenumber'>*4955</h1>
+  <div className= 'navbuttons'>
+  <button className='navbtn1'> התנדבות</button> 
+  <button className='navbtn2'> אימוץ כלב</button>
+  <button className='navbtn3'> חדשות</button>
+  <button className='navbtn4'> צרו קשר</button>
+  <button className='navbtn5'> תרומה</button>
+  </div>
+   </div>
+</nav>
+       
+        {/* <Navbar className=>
+          <Container className='navitems'>
+  
+  >
+
+  </div>
+          </Container>
+        </Navbar> */}
+      </>
+    );
+  }
+  
+  export default MainNavbar
+
+
+
+
